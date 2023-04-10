e68fcca9f310db2f429962a34fe4c58cf15df6a5
ChristopherVuvu <vuvumumo93@gmail.com>
2023-04-10 07:46:34 +0300

    added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+//Declaring the variables to use
 let acc_balance = document.getElementById('acc-balance');
 let input = document.getElementById('input-box');
 let prevBalance =0;
 
 document
-    .getElementsByClassName("Withdrawl-btn")
+    .getElementsByClassName("Withdrawal-btn")
     .addEventListener('click', handleWithdrawalTransactions);
 
+//setting the fuction on the initial balance value of the account
 function setInitialBalance(){
     let url = "http://localhost:3000/transaction-history";
     fetch(url, {method: "GET"})
@@ -16,38 +18,37 @@ function setInitialBalance(){
     });
 }
 
+//set a function for deposit transactions
+function handleDeposit(){
+let amount = input.value;
+let d = new Date(Date.now());
+if(amount < 0 || amount === ""){
+    alert("Amount must be greator than zero");
+    return;
+}
 
+document
+    .getElementsByClassName('deposit-btn')
+    .addEventListener('click', handleDeposit);
+//determine the trancation types to done by this function
+let data = {
+date: d,
+transaction_type: "Deposit",
+amount: amount,
+balance: prevBalance
+}
 
+data = JSON.stringify(data);
 
+let url = "http://localhost:3000/transaction-history";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-body: data,
-headers: {
-    "content-Type" : "application/json",
-},
+//fetch the deposit transactions via POST in Json API
+fetch(url, {
+    method: "POST",
+    body: data,
+    headers: {
+        "content-Type": "application/json",
+    },
 })
     .then((response)=>response.json())
     .then((data)=> {
@@ -58,74 +59,57 @@ headers: {
     });
 }
 
+//set up a function to handle withdrawal transactions
 function handleWithdrawalTransactions(){
     let amount = input.value;
     let d = new Date(Date.now());
+    if(amount < 0 || amount === ""){
+        alert("Amount must be greator than zero");
+        return;
+    }
+
+//the data below is set for withdrawal transaction type
+let data = {
+    date: d,
+    transaction_type: "Withdraw",
+    amount: amount,
+    balance: prevBalance
 }
 
+//fetch the withdrawal transactions using POST in Json
+fetch(url, {
+    method: "POST",
+    body: data,
+    headers: {
+        "content-Type": "application/json",
+    }
+})
+    .then((response) => response.json())
+    .then((data)=> {
+        console.log(data);
+        input.value ="";
+        updateBalance(parseInt(prevBalance)-parseInt(amount));
+        alert(`Amount Withdrawn`)
+    })
+}
 
+//set up a function to update the account balance depended
+//on type of transaction done
+function updateBalance(amount){
+    let data = {
+        amount: amount,
+        id: 0,
+    };
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-data = JSON.stringify(data);
+    data = JSON.stringify(data);
 
 let url = "http://localhost:3000/transaction-history";
 fetch(url, {
     method: "PUT",
     body: data,
     header:{
-        "content-Type": "applicatioj/json",
+        "content-Type": "application/json",
     },
 })
     .then((response) => response.json())
@@ -134,7 +118,32 @@ fetch(url, {
         setInitialBalance();
     })
 
+//set a function to populate and update a transaction 
+//on the browser once a transcation is done
+function populateTable(){
+    let url: "http://localhost:3000/transaction-history";
 
+    <tr>
+        <th scope="row">${doc.id}</th>
+        <td>${doc.date}</td>
+        <td>${doc.amount}</td>
+        <td>${doc.transaction_type}</td>
+        <td>${doc.balance}</td>
+    </tr>
 
+    fetch(url), {
+        method: "GET",
+        headers:{
+            "content-TYpe": "Application/json";
+        }
+    }
+    .then((response)=> response.json())
+    .then((data)=> {
+        let element = "";
+        for (let doc of data)(){
+            element = element+""
+        }
+    });
+}
 
-
+setBalance();
